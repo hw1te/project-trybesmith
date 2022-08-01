12c98634a760b5bd226f032a0f4959ae91fc16ee
Gustavo Pospi <gutxander@gmail.com>
2022-08-01 16:54:45 -0300

    fix: refatora query requisito 4.

--- src/models/ordersModel.ts
+++ src/models/ordersModel.ts
@@ -4,8 +4,8 @@ import connection from './connection';
 const ordersModel = {
   getAll: async (): Promise<RowDataPacket[]> => {
     const [orders] = await connection.query<RowDataPacket[]>(`
-    SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) as productsIds
-    FROM Trybesmith.Products as p INNER JOIN Trybesmith.Orders as o
+    SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS productsIds
+    FROM Trybesmith.Products AS p INNER JOIN Trybesmith.Orders AS o
     ON p.orderId = o.id GROUP BY o.id, o.userId ORDER BY o.userId;
   `);
 
